792e219b209f79fc4c7d3437a70f6c6b994a6b2c
aly ba <alybas2002@yahoo.fr>
2014-03-21 10:28:45 +0100

    c# avancé en cour

---
+++ src/src/Lesson01/Lesson01Test/PointTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lesson01;
+
+namespace Lesson01Test
+{
+    [TestClass]
+    public class PointTests
+    {
+        [TestMethod]
+        public void PointAddition()
+        {
+            var sum = 1 + 2;
+            var foo = "hello" + "world";
+            var bar = foo + sum;
+
+            Console.WriteLine(bar);
+            
+        }
+
+    }
+}
